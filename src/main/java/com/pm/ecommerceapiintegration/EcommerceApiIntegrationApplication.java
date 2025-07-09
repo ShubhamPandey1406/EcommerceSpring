adff95dee7b027e007aea48ec960d7bae760fe25
Shubham <shwillshine@gmail.com>
2025-07-09 23:42:13 +0530

    connected to database

--- src/main/java/com/pm/ecommerceapiintegration/EcommerceApiIntegrationApplication.java
+++ src/main/java/com/pm/ecommerceapiintegration/EcommerceApiIntegrationApplication.java
@@ -3,8 +3,10 @@ package com.pm.ecommerceapiintegration;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class EcommerceApiIntegrationApplication {
 
     public static void main(String[] args) {
